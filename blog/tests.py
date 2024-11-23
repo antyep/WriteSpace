d7b114ae57d2d231603f24754d3fd4ad33cb8790
antyep <antyep6@gmail.com>
2024-11-23 23:15:43 +0100

    feat: add test with user data

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@email.com",
+            password="secretpassword123",
+        )
