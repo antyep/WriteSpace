71b5ef496e2e86da251e50feed306d17f566efb7
antyep <antyep6@gmail.com>
2024-11-23 23:39:40 +0100

    test: add test for urls and absolute urls

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.test import TestCase
 from .models import Post
 
@@ -16,13 +17,24 @@ class BlogTests(TestCase):
 
         cls.post = Post.objects.create(
             title="A good title",
-            body="Creating nice body for you",
+            body="Creating a nice body for you",
             author=cls.user,
         )
 
-        def test_post_model(self):
-            self.assertEqual(self.post.title, "A good title")
-            self.assertEqual(self.post.body, "Creating a nice body for you")
-            self.assertEqual(self.post.author.username, "testuser")
-            self.assertEqual(str(self.post), "A good title")
-            self.assertEqual(self.post.get_absolute_url(), "/post/1/")
+    def test_post_model(self):
+        self.assertEqual(self.post.title, "A good title")
+        self.assertEqual(self.post.body, "Creating a nice body for you")
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(str(self.post), "A good title")
+        self.assertEqual(self.post.get_absolute_url(), "/post/1")
+
+    def test_url_exists_at_correct_location_listview(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exists_at_correct_location_detailview(self):
+        response = self.client.get("/post/1/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_listview(self):
+        response = self.client.get(reverse("home"))
