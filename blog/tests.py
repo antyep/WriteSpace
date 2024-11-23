c0283dc9571d0deede877a2877ee28d79005b9b9
antyep <antyep6@gmail.com>
2024-11-23 23:22:13 +0100

    test: add test cases for model and absolute URL

--- blog/tests.py
+++ blog/tests.py
@@ -19,3 +19,10 @@ class BlogTests(TestCase):
             body="Creating nice body for you",
             author=cls.user,
         )
+
+        def test_post_model(self):
+            self.assertEqual(self.post.title, "A good title")
+            self.assertEqual(self.post.body, "Creating a nice body for you")
+            self.assertEqual(self.post.author.username, "testuser")
+            self.assertEqual(str(self.post), "A good title")
+            self.assertEqual(self.post.get_absolute_url(), "/post/1/")
