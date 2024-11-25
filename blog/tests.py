1329836d95a516aa3b77799b855e9b1fcf101715
antyep <antyep6@gmail.com>
2024-11-25 11:33:14 +0100

    test: add post delete test

--- blog/tests.py
+++ blog/tests.py
@@ -76,3 +76,8 @@ class BlogTests(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Post.objects.last().title, "Updated title")
         self.assertEqual(Post.objects.last().body, "Updated text")
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse("post_delete", args="1"))
+
+        self.assertEqual(response.status_code, 302)
