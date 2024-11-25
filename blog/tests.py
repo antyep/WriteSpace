0f5c2facdd566a1f9667ca65ef43570e11d6577b
antyep <antyep6@gmail.com>
2024-11-25 11:28:29 +0100

    test: add post update test

--- blog/tests.py
+++ blog/tests.py
@@ -63,3 +63,16 @@ class BlogTests(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Post.objects.last().title, "New title")
         self.assertEqual(Post.objects.last().body, "New text")
+
+    def test_post_update_view(self):
+        response = self.client.post(
+            reverse("post_edit", args="1"),
+            {
+                "title": "Updated title",
+                "body": "Updated text",
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Updated title")
+        self.assertEqual(Post.objects.last().body, "Updated text")
